52f6c44498033b2b0904c26c2fcd0977e8338ab2
cchen97 <manjic@uw.edu>
2019-01-17 16:17:05 -0800

    almost done

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -23,6 +23,66 @@ class Calculator {
     }
     
     func divide (lhs: Int, rhs: Int) -> Int {
-        return lhs + rhs
+        return lhs / rhs
     }
+    
+    func add (_ list: [Int]) -> Int {
+        if list.isEmpty {
+            return 0
+        } else {
+            var result = 0
+            for i in list {
+                result = result + i
+            }
+            return result
+         }
+    }
+    
+    func multiply (_ list: [Int]) -> Int {
+        if list.isEmpty {
+            return 0
+        } else {
+            var result = 1
+            for i in list {
+                result = result * i
+            }
+            return result
+        }
+    }
+    
+    func count (_ list: [Int]) -> Int {
+        return list.count
+    }
+    
+    func avg (_ list: [Int]) -> Int {
+        if list.isEmpty {
+            return 0
+        }
+        var result = 0
+        for i in list {
+            result = result + i
+        }
+        return result / list.count
+    }
+    
+    func mathOp (lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs)
+    }
+    
+    func mathOp (args: [Int], beg: Int, op: (Int, Int) -> Int) -> Int {
+        var result = op(beg, args[0])
+        for i in 1..<args.count {
+            result = op(result, args[i])
+        }
+        return result
+    }
+    
+    func add (lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 + rhs.0, lhs.1 + rhs.1)
+    }
+    
+    func subtract (lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 - rhs.0, lhs.1 - rhs.1)
+    }
+    
 }
